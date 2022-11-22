de350d13956277a4f0677fae147881b2a73f495e
Carlos ramos <devcarlosramos@gmail.com>
2022-11-21 19:09:50 -0500

    configuracion de la base de datos rama main

---
+++ src/main/java/ProyectIdea/ProyectIdeaApplication.java
@@ -0,0 +1,13 @@
+package ProyectIdea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectIdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectIdeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProyectIdea/ProyectIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectIdea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectIdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
